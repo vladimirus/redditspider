7d94fe008a4370142f3e4ab5558192e92f80b5cd
Vladmir Parshikov <v-vparshikov@expedia.com>
2014-10-30 16:49:27 +0000

    removed not need legacy stuff

--- src/main/java/com/redditspider/dao/browser/WebBrowserUtils.java
+++ src/main/java/com/redditspider/dao/browser/WebBrowserUtils.java
@@ -1,13 +1,10 @@
 package com.redditspider.dao.browser;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.openqa.selenium.firefox.FirefoxDriver.PROFILE;
 import static org.openqa.selenium.remote.DesiredCapabilities.firefox;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
  * Utils for webBrowser.
@@ -19,17 +16,7 @@ public final class WebBrowserUtils {
     }
 
     public static WebDriver aFirefoxDriver() {
-        int domMaxChromeScriptRunTime = 4500;
-        int domMaxScriptRunTime = 3500;
-
-        FirefoxProfile firefoxProfile = new FirefoxProfile();
-        firefoxProfile.setPreference("dom.max_chrome_script_run_time", domMaxChromeScriptRunTime);
-        firefoxProfile.setPreference("dom.max_script_run_time", domMaxScriptRunTime);
-
-        DesiredCapabilities capabilities = firefox();
-        capabilities.setCapability(PROFILE, firefoxProfile);
-
-        WebDriver firefoxDriver = new FirefoxDriver(capabilities);
+        WebDriver firefoxDriver = new FirefoxDriver(firefox());
         firefoxDriver.manage().timeouts().implicitlyWait(60, SECONDS);
         firefoxDriver.manage().timeouts().pageLoadTimeout(60, SECONDS);
         firefoxDriver.manage().timeouts().setScriptTimeout(60, SECONDS);
