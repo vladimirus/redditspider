62d3a964118574c54a02bddf33baa07224b4ee50
vov <vov@vov-PC>
2014-11-09 18:27:08 +0000

    disabled debugging

--- src/main/java/com/redditspider/biz/manager/impl/RedditManagerImpl.java
+++ src/main/java/com/redditspider/biz/manager/impl/RedditManagerImpl.java
@@ -5,9 +5,6 @@ import static org.springframework.util.StringUtils.hasText;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
-import org.joda.time.DateTime;
-import org.joda.time.Duration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,20 +19,13 @@ import com.redditspider.model.reddit.SearchResult;
  */
 @Service
 public class RedditManagerImpl implements SearchManager {
-    private static final transient Logger LOG = Logger.getLogger(RedditManagerImpl.class);
+//    private static final transient Logger LOG = Logger.getLogger(RedditManagerImpl.class);
     @Autowired
     SearchDao searchDao;
 
     public List<Link> findLinks(SearchQuery query) {
         List<Link> links = newArrayList();
-        DateTime start = new DateTime();
-        LOG.debug(start.toString() + "; starting search: " + query.getSearchUri());
-        links = findNewLinks(query, links);
-        DateTime end = new DateTime();
-        Duration dur = new Duration(start, end);
-        dur.toString();
-        LOG.debug(end.toString() + "; finished search: " + query.getSearchUri() + "; duration: " + dur.toString() + "; links: " + links.size());
-        return links;
+        return findNewLinks(query, links);
     }
 
     private List<Link> findNewLinks(SearchQuery query, List<Link> links) {
