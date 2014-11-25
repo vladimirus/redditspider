60b4aafbe688d4118eb33ef874a531c3ebd0cfdf
Vladmir Parshikov <v-vparshikov@expedia.com>
2014-11-25 15:12:39 +0000

    added user cache

--- src/main/java/com/redditspider/dao/reddit/api/RedditApiUserManager.java
+++ src/main/java/com/redditspider/dao/reddit/api/RedditApiUserManager.java
@@ -1,10 +1,16 @@
 package com.redditspider.dao.reddit.api;
 
+import static java.util.concurrent.TimeUnit.MINUTES;
+
 import com.github.jreddit.entity.User;
 import com.github.jreddit.utils.restclient.RestClient;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.Callable;
 
 /**
  * Reddit's api to connect to user account.
@@ -18,8 +24,26 @@ public class RedditApiUserManager {
     @Autowired
     RestClient restClient;
 
-    //TODO: implement caching
+    Cache<String, User> cache;
+
+    public RedditApiUserManager() {
+        this.cache = CacheBuilder.newBuilder()
+                .maximumSize(5)
+                .expireAfterWrite(10, MINUTES)
+                .build();
+    }
+
     User getUser() throws Exception {
+        return cache.get("user", new Callable<User>() {
+            @Override
+            public User call() throws Exception {
+                return getUserFromApi();
+            }
+        });
+
+    }
+
+    private User getUserFromApi() throws Exception {
         User user = new User(restClient, username, password);
         user.connect();
         return user;
