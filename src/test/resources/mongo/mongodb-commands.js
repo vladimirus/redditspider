6d6ae4b89b1fb0aad45f3e49fbc848d47af9daf6
Vladmir Parshikov <v-vparshikov@expedia.com>
2014-11-26 11:49:50 +0000

    added useful comman

--- src/test/resources/mongo/mongodb-commands.js
+++ src/test/resources/mongo/mongodb-commands.js
@@ -1,2 +1,3 @@
 db.subreddit.find({}, {'name': 1});
 db.subreddit.find({}, {'name': 1, 'updated': 1, 'subscribers': 1, _id: 0}).sort({updated: -1}).limit(1000);
+db.subreddit.count({ subscribers: { $gt: 100 } });
